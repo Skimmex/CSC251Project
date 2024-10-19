3f32726285e7e810f3a3edca5665a11a79215ee9
Skimmex <brandon.lai37@gmail.com>
2024-10-19 17:39:54 -0400

    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -30,43 +30,43 @@ public class Policy
       holderHeight = height;
       holderWeight = weight;
    }
-   
+   //@return policyNumber
    public int getPolicyNumber(){
       return policyNumber;
    }   
-   
+   //@return holderFirstName
    public String getHolderFirstName(){
       return holderFirstName;
    }   
-   
+   //@return providerName
    public String getProviderName(){
       return providerName;
    }   
-   
+   //@return holderLastName
    public String getHolderLastName(){
       return holderLastName;
    }   
-   
+   //@return holderAge
    public int getHolderAge(){
       return holderAge;
    }   
-   
+   //@return holderSmokes
    public String getHolderSmokes(){
       return holderSmokes;
    } 
-   
+   //@return holderHeight
    public double getHolderHeight(){
       return holderHeight;
    } 
-   
+   //@return holderWeight
    public double getHolderWeight(){
       return holderWeight;
    } 
-   
+   //@return BMi derived from holder's weight and height
    public double getBMI(){
       return (holderWeight*703)/(holderHeight*holderHeight);
    }
-   
+   //@return price of policy based on smoking and bmi conditions
    public double getPrice(){
       int baseFee = 600;
       if (holderAge > 50){
