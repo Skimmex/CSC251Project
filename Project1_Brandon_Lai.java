55a7f335bca5b3488523f509ef2584f253a73e8b
Skimmex <brandon.lai37@gmail.com>
2024-10-20 14:30:13 -0400

    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project1_Brandon_Lai.java
+++ Project1_Brandon_Lai.java
@@ -1,41 +1,50 @@
 import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
 public class Project1_Brandon_Lai
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws Exception
    {
-      Scanner scnr = new Scanner(System.in);
+      File file = new File("PolicyInformation.txt");
+      Scanner scnr = new Scanner(file);
+      ArrayList<Policy> policies = new ArrayList<>();
+      int smokers = 0;
+      int nonsmokers = 0;
+      while (scnr.hasNextLine())
+      {
+         int number = scnr.nextInt();
+         scnr.nextLine();
+         String name = scnr.nextLine();
+         String fName = scnr.nextLine();
+         String lName = scnr.nextLine();
+         int age = scnr.nextInt();
+         scnr.nextLine();
+         String smoker = scnr.nextLine();
+         if (smoker.equals("smoker")) smokers++;
+         else nonsmokers++;
+         double height = scnr.nextDouble();
+         double weight = scnr.nextDouble();
+         
+         Policy policy = new Policy(number,name,fName,lName,age,smoker,height,weight);
+         
+         System.out.println("Policy Number: " + policy.getPolicyNumber());
+         System.out.println("Provider Name: " + policy.getProviderName());
+         System.out.println("Policyholder’s First Name: " + policy.getHolderFirstName());
+         System.out.println("Policyholder’s Last Name: " + policy.getHolderLastName());
+         System.out.println("Policyholder’s Age: " + policy.getHolderAge());
+         System.out.println("Policyholder’s Smoking Status: " + policy.getHolderSmokes());
+         System.out.println("Policyholder’s Height: " + policy.getHolderHeight() + " inches");
+         System.out.println("Policyholder’s Weight: " + policy.getHolderWeight()+ " pounds");
+         System.out.printf("Policyholder’s BMI: %.2f", policy.getBMI());
+         System.out.printf("\nPolicy Price: $%.2f", policy.getPrice());
+         System.out.println("\n");
+         
+      }
+      scnr.close();
       
-      System.out.println("Please enter the Policy Number: ");
-      int number = scnr.nextInt();
-      scnr.nextLine();
-      System.out.println("Please enter the Provider Name: ");
-      String name = scnr.nextLine();
-      System.out.println("Please enter the Policyholder’s First Name: ");
-      String fName = scnr.nextLine();
-      System.out.println("Please enter the Policyholder’s Last Name: ");
-      String lName = scnr.nextLine();
-      System.out.println("Please enter the Policyholder’s Age: ");
-      int age = scnr.nextInt();
-      scnr.nextLine();
-      System.out.println("Please enter the Policyholder’s Smoking Status (smoker/non-smoker): ");
-      String smoker = scnr.nextLine();
-      System.out.println("Please enter the Policyholder’s Height (in inches): ");
-      double height = scnr.nextDouble();
-      System.out.println("Please enter the Policyholder’s Weight (in pounds): ");
-      double weight = scnr.nextDouble();
+      System.out.println("The number of policies with a smoker is: "+smokers);
+      System.out.println("the number of policies with a non-smoker is: "+nonsmokers);
       
-      Policy policy = new Policy(number,name,fName,lName,age,smoker,height,weight);
-      
-      System.out.println("Policy Number: " + policy.getPolicyNumber());
-      System.out.println("Provider Name: " + policy.getProviderName());
-      System.out.println("Policyholder’s First Name: " + policy.getHolderFirstName());
-      System.out.println("Policyholder’s Last Name: " + policy.getHolderLastName());
-      System.out.println("Policyholder’s Age: " + policy.getHolderAge());
-      System.out.println("Policyholder’s Smoking Status: " + policy.getHolderSmokes());
-      System.out.println("Policyholder’s Height: " + policy.getHolderHeight() + " inches");
-      System.out.println("Policyholder’s Weight: " + policy.getHolderWeight()+ " pounds");
-      System.out.println("Policyholder’s BMI: " + policy.getBMI());
-      System.out.println("Policy Price: $%.2d" + policy.getPrice());
       
       
       
